757b8fe5ddeae676a3fb53d98fdff4244f4173d8
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2023-08-04 14:59:38 +0200

    update tests

--- xcmor/tests/test_utils.py
+++ xcmor/tests/test_utils.py
@@ -1,6 +1,7 @@
 import pytest
 
-from ..utils import parse_cell_methods
+from ..utils import filter_table_by_value, parse_cell_methods
+from .tables import mip_amon
 
 
 @pytest.mark.parametrize(
@@ -14,3 +15,17 @@ from ..utils import parse_cell_methods
 def test_parse_cell_methods(cell_methods, expected):
     parsed = parse_cell_methods(cell_methods)
     assert parsed == expected
+
+
+def test_filter_by_value():
+    filtered = filter_table_by_value(mip_amon, "standard_name", "air_temperature")
+    expected = ["ta", "tas"]
+    assert list(filtered.keys()) == expected
+    for k, v in filtered.items():
+        assert v == mip_amon[k]
+
+    filtered = filter_table_by_value(mip_amon, "standard_name", "precipitation_flux")
+    expected = ["pr"]
+    assert list(filtered.keys()) == expected
+    for k, v in filtered.items():
+        assert v == mip_amon[k]
--- xcmor/tests/test_xcmor.py
+++ xcmor/tests/test_xcmor.py
@@ -1,62 +1,6 @@
 from ..datasets import reg_ds
 from ..xcmor import add_variable_attrs, cmorize
-
-mip_amon = {
-    "ta": {
-        "frequency": "mon",
-        "modeling_realm": "atmos",
-        "standard_name": "air_temperature",
-        "units": "K",
-        "cell_methods": "time: mean",
-        "cell_measures": "area: areacella",
-        "long_name": "Air Temperature",
-        "comment": "Air Temperature",
-        "dimensions": "longitude latitude plev19 time",
-        "out_name": "ta",
-        "type": "real",
-        "positive": "",
-        "valid_min": "",
-        "valid_max": "",
-        "ok_min_mean_abs": "",
-        "ok_max_mean_abs": "",
-    },
-    "tas": {
-        "frequency": "mon",
-        "modeling_realm": "atmos",
-        "standard_name": "air_temperature",
-        "units": "K",
-        "cell_methods": "area: time: mean",
-        "cell_measures": "area: areacella",
-        "long_name": "Near-Surface Air Temperature",
-        "comment": "near-surface (usually, 2 meter) air temperature",
-        "dimensions": "longitude latitude time height2m",
-        "out_name": "tas",
-        "type": "real",
-        "positive": "",
-        "valid_min": "",
-        "valid_max": "",
-        "ok_min_mean_abs": "",
-        "ok_max_mean_abs": "",
-    },
-    "pr": {
-        "frequency": "mon",
-        "modeling_realm": "atmos",
-        "standard_name": "precipitation_flux",
-        "units": "kg m-2 s-1",
-        "cell_methods": "area: time: mean",
-        "cell_measures": "area: areacella",
-        "long_name": "Precipitation",
-        "comment": "includes both liquid and solid phases",
-        "dimensions": "longitude latitude time",
-        "out_name": "pr",
-        "type": "real",
-        "positive": "",
-        "valid_min": "",
-        "valid_max": "",
-        "ok_min_mean_abs": "",
-        "ok_max_mean_abs": "",
-    },
-}
+from .tables import mip_amon
 
 
 def test_add_variable_attrs():
@@ -90,5 +34,3 @@ def test_cmorize():
             assert da.attrs[k] == mip_table[var][k]
         for k in expected_global_attrs:
             assert ds_out.attrs[k] == mip_table[var][k]
-
-    return ds_out
