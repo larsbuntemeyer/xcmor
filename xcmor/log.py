269b912b9cd87dbb90294a4d2af0f90b32546d97
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2023-08-13 22:22:27 +0200

    added log module

---
+++ xcmor/log.py
@@ -0,0 +1,39 @@
+import logging
+
+
+class CustomFormatter(logging.Formatter):
+    blue = "\x1b[38;5;39m"
+    bright_blue = "\u001b[34;1m"
+    bright_green = "\u001b[32;1m"
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = (
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    )
+
+    FORMATS = {
+        logging.DEBUG: yellow + format + reset,
+        logging.INFO: bright_green + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset,
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
+def get_logger(name):
+    # create console handler with a higher log level
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(CustomFormatter())
+    logger.addHandler(ch)
+    return logger
