4dedd8851c22722a582f40da01c8c95fef984f57
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2024-03-04 12:20:11 +0100

    added test

---
+++ xcmor/tests/test_rules.py
@@ -0,0 +1,24 @@
+import xarray as xr
+
+from ..rules import rules
+
+
+def test_rules():
+    da = xr.DataArray(
+        data=[1, 4, 2, 9],
+        dims=("x"),
+        coords={"x": [0, 1, 2, 3]},
+    )
+    da.attrs["type"] = "float64"
+    da.name = "variable"
+    da.attrs = {
+        "coordinates": "longitude",
+        "type": "real",
+        "out_name": "tas",
+        "valid_min": 0,
+        "valid_max": 10,
+        "standard_name": "air_temperature",
+    }
+    for attr in da.attrs:
+        if hasattr(rules, attr):
+            da = getattr(rules, attr)(da)
